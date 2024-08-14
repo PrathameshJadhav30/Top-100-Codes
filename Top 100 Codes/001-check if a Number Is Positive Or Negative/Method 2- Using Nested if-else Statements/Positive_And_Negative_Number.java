9920cb2774734988df5b01ed8f57a440a5b954c9
Prathamesh <prathameshaj2004@gmail.com>
2024-08-14 07:56:24 +0530

    Changes

--- Top 100 Codes/001-check if a Number Is Positive Or Negative/Method 2- Using Nested if-else Statements/Positive_And_Negative_Number.java
+++ Top 100 Codes/001-check if a Number Is Positive Or Negative/Method 2- Using Nested if-else Statements/Positive_And_Negative_Number.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Positive_And_Negative_Number {
     public static void main(String[] args) {
+        //using Nested If-else Statement
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+
+        //Condition to check if the number is positive or negative
+        if(num >=0)
+        {
+            if(num == 0)
+                System.out.println("Zero");
+            else 
+                System.out.println("The number is positive");
+
+        }
+        else
+            System.out.println("The number is Negative");
+
+        sc.close();
         
     }
     
