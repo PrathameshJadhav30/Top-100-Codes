afa8138500218b7fc63b31b6c8f5af79eec537b9
Prathamesh <prathameshaj2004@gmail.com>
2024-08-14 08:08:04 +0530

    Changes

---
+++ Top 100 Codes/001-check if a Number Is Positive Or Negative/Method 2- Using Nested if-else Statements/PositiveAndNegativeNumber.py
@@ -0,0 +1,9 @@
+#using Nested if-else Statement
+num = int(input("Enter A number: "))
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive")
+else:
+    print("Negative")
