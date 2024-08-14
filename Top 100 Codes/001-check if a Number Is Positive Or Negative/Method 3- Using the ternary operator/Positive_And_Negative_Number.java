27e15b16d72c7516386af9acc885f12d557265ce
Prathamesh <prathameshaj2004@gmail.com>
2024-08-14 08:01:20 +0530

    Changes

--- Top 100 Codes/001-check if a Number Is Positive Or Negative/Method 3- Using the ternary operator/Positive_And_Negative_Number.java
+++ Top 100 Codes/001-check if a Number Is Positive Or Negative/Method 3- Using the ternary operator/Positive_And_Negative_Number.java
@@ -1,8 +1,23 @@
-
+import java.util.Scanner;
 
 public class Positive_And_Negative_Number {
     public static void main(String[] args) {
-        
+        //using Ternary Operator
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num =sc.nextInt();
+
+        //Condition to check if the number is negative or positive
+
+        if (num ==0)
+        {
+            System.out.println("Zero");
+        }
+        else{
+            String result = num > 0 ?"The number is Positive":"The number is negative";
+            System.out.println(result);
+        }
+        sc.close();
     }
     
 }
