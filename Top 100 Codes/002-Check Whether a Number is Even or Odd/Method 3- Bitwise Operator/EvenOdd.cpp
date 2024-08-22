48e4464b3510d8e25389a6c234cf417ab68bb283
Prathamesh <prathameshaj2004@gmail.com>
2024-08-22 23:07:09 +0530

    Changes

---
+++ Top 100 Codes/002-Check Whether a Number is Even or Odd/Method 3- Bitwise Operator/EvenOdd.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//Return true if n is even, else odd
+bool isEven(int number)
+{
+    //n & 1 is 1, then odd, else even
+    return (!(number & 1));
+}
+
+//Driver code
+int main()
+{
+    int number;
+
+    cout<<"Enter the number : ";
+    cin >> number;
+    if (isEven(number))
+      cout << "Even";
+    else 
+       cout << "Odd";
+
+    //below can also be used instead of if else conditions
+    //isEven(number)? cout << "Even": cout << "odd";
+    return 0;
+}
