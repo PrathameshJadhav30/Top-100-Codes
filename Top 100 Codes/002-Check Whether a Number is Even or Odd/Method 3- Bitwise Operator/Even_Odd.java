86061dce0fec30e19c265d2811484907a8c8519f
Prathamesh <prathameshaj2004@gmail.com>
2024-08-22 23:15:47 +0530

    Changes

--- Top 100 Codes/002-Check Whether a Number is Even or Odd/Method 3- Bitwise Operator/Even_Odd.java
+++ Top 100 Codes/002-Check Whether a Number is Even or Odd/Method 3- Bitwise Operator/Even_Odd.java
@@ -1,6 +1,21 @@
 
 public class Even_Odd {
-    public static void main(String[] args) {
-        
-    }
+    public static void main (String[]args)
+  {
+    int number = 29;
+
+    if (isEven (number))
+        System.out.println ("Even");
+    else
+        System.out.println ("Odd");
+  }
+
+
+// Returns true if n is even, else odd
+  static bool isEven (int number)
+  {
+
+    // n & 1 is 1, then odd, else even
+    return (!(number & 1));
+  }
 }
