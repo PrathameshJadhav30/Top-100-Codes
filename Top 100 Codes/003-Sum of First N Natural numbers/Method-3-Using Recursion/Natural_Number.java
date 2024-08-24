fd224045341563960bc3739c0c7ec563f0c62ab0
Prathamesh <prathameshaj2004@gmail.com>
2024-08-24 10:25:32 +0530

    Changes

--- Top 100 Codes/003-Sum of First N Natural numbers/Method-3-Using Recursion/Natural_Number.java
+++ Top 100 Codes/003-Sum of First N Natural numbers/Method-3-Using Recursion/Natural_Number.java
@@ -1,8 +1,25 @@
-
+import java.util.Scanner;
 
 public class Natural_Number {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n = sc.nextInt();
+
+        int sum = getsum(n);
+
+           System.out.println(sum);
         
+           sc.close();
+        
+    }
+
+    static int getsum(int n)
+    {
+        if( n == 0)
+          return 0;
+
+        return n + getsum(n -1);
     }
     
 }
