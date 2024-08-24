e7cd5dacf87a48fad75572663115dbc1c77fa62b
Prathamesh <prathameshaj2004@gmail.com>
2024-08-24 10:21:50 +0530

    Changes

--- Top 100 Codes/003-Sum of First N Natural numbers/Method-2-Using Formula for the Sum of Nth Term/Natural_Number.java
+++ Top 100 Codes/003-Sum of First N Natural numbers/Method-2-Using Formula for the Sum of Nth Term/Natural_Number.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Natural_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n = sc.nextInt();
+
+        System.out.println(n*(n+1)/2);
+
+        sc.close();
+    }
     
 }
